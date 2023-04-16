0b44f26847924ff36dd58055a9dc9d3b3b822f2a
albani07 <alban07halaj@gmail.com>
2023-04-16 12:16:07 +0200

    applying logic to find the number

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 'use strict'
 
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = number;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
  
 document.querySelector('.check').addEventListener('click', function() {
 	const guess = Number(document.querySelector('.guess').value);
 	if(!guess){
 		document.querySelector('.message').textContent = 'No number!';
+	}else if (guess === secretNumber){
+		document.querySelector('.message').textContent = 'Correct number!'
+	}else if(guess > secretNumber){
+		document.querySelector('.message').textContent = 'Too High!'
+	}else if(guess < secretNumber){
+		document.querySelector('.message').textContent = 'Too Low!'
 	}
 });
